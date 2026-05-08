792b49c3dabb5842a4c9427dc3848379a7891d9e
Logan Dhillon <dev@logandhillon.com>
2026-05-08 17:32:36 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function onLoad() {
+  document.getElementById("copyright").textContent = `© 2025-${new Date().getFullYear()} Reversal Studios. All rights reserved.`;
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
